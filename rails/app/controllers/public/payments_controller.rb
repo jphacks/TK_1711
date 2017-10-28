8cabd78bf0a144b4d953848deb35702006b58c06
keke <19yamashita15@gmail.com>
2017-10-28 22:54:30 +0900

    add seed and photo

--- rails/app/controllers/public/payments_controller.rb
+++ rails/app/controllers/public/payments_controller.rb
@@ -8,7 +8,7 @@ class Public::PaymentsController < Public::BaseController
 
     def create
         @user = User.find_by(address:params[:payment][:sender])
-        @container = @user.containers.build(name:"卵",date:"2017-11-2",duration:2,status:0)
+        @container = @user.containers.build(name:"卵",date:"2017-11-2",photo:"https://s3-ap-northeast-1.amazonaws.com/myblock/egg.jpg",uration:2,status:0)
         @payment = @container.build_payment(sender:params[:payment][:sender],shop:"0x0b9d052652f94547464746a08e8281cd7aa6e059",mount:100)
     
         if @payment.save
