8c81b5a7bca8f979741a32d808598e72be5c505e
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-10-28 11:54:58 +0900

    Add vc for list

---
+++ ios-client/my-block/my-block/ViewController/DeliveryListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DeliveryListViewController.swift
+//  my-block
+//
+//  Created by Masaya Hayashi on 2017/10/28.
+//  Copyright © 2017年 Masaya Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class DeliveryListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
