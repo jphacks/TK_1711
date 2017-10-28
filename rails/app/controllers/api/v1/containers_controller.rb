472f6ac7b91ee2f093875822028d4cc5f89b284f
keke <19yamashita15@gmail.com>
2017-10-29 05:43:10 +0900

    add transaction

--- rails/app/controllers/api/v1/containers_controller.rb
+++ rails/app/controllers/api/v1/containers_controller.rb
@@ -21,9 +21,34 @@ class Api::V1::ContainersController < Api::V1::BaseController
         duration = params[:duration].to_i
         status = params[:status].to_i
 
-        @container.delivery_date = delivery_date
-        @container.duration = duration
-        @container.status = status
+        if @container.status == status 
+            # 3ならばそのままで放置。
+            puts "This is already finished"
+        elsif status == 3
+            @container.delivery_date = delivery_date
+            @container.duration = duration
+            @container.status = status
+
+            # Status 3になったときに契約書を参照する。
+
+            @payment = @container.payment
+
+            sender_addr = @payment.sender
+            shop_addr = @payment.shop
+            mount = @payment.mount
+
+            @sender = User.find_by(address:sender_addr)
+            @shop = User.find_by(address:shop_addr)
+            @sender.balance -= mount
+            @shop.balance += mount 
+            @sender.save
+            @shop.save
+        else
+            # 従来が3以外で、params[：id]が３ではないときは更新。
+            @container.delivery_date = delivery_date
+            @container.duration = duration
+            @container.status = status
+        end
 
         if @container.save
             render :json => @container.to_json
