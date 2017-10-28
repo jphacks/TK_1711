fdc769633fbdadce2cbf06338d7eebb416265720
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-10-28 11:50:45 +0900

    Create my-block project

---
+++ ios-client/my-block/my-block/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my-block
+//
+//  Created by Masaya Hayashi on 2017/10/28.
+//  Copyright © 2017年 Masaya Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
