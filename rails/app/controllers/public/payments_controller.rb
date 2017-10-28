b8c19fa6017a0a0498ca0e6a021337cda8dfce77
keke <19yamashita15@gmail.com>
2017-10-29 05:08:43 +0900

    fix egg to gura

--- rails/app/controllers/public/payments_controller.rb
+++ rails/app/controllers/public/payments_controller.rb
@@ -8,7 +8,7 @@ class Public::PaymentsController < Public::BaseController
 
     def create
         @user = User.find_by(address:params[:payment][:sender])
-        @container = @user.containers.build(name:"卵",delivery_date:"2017-11-2", purchase_date:"2017-11-2", photo:"https://s3-ap-northeast-1.amazonaws.com/myblock/egg.jpg", duration:2,status:0)
+        @container = @user.containers.build(name:"グラノーラ",delivery_date:"2017-11-2", purchase_date:"2017-11-2", photo:"https://s3-ap-northeast-1.amazonaws.com/myblock/gura.png", duration:2,status:0)
         @payment = @container.build_payment(sender:params[:payment][:sender],shop:"0x0b9d052652f94547464746a08e8281cd7aa6e059",mount:100)
     
         if @payment.save
