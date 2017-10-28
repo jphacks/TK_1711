e9b173e1d257da64ea373da08da33343a35c4bc1
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-10-28 12:35:05 +0900

    Show deliveries

--- ios-client/my-block/my-block/ViewController/DeliveryListViewController.swift
+++ ios-client/my-block/my-block/ViewController/DeliveryListViewController.swift
@@ -12,8 +12,40 @@ class DeliveryListViewController: UIViewController {
 
     @IBOutlet private weak var deliveryListTableView: UITableView!
 
+    private var deliveries: [Delivery] = [] {
+        didSet {
+            deliveryListTableView.reloadData()
+        }
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        GetDeliveriesService().request(URLSession.shared) { result in
+            switch result {
+            case .success(let res):
+                self.deliveries = res.deliveries
+            case .failure(let err):
+                print(err)
+            }
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+}
+
+extension DeliveryListViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return deliveries.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = deliveryListTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = deliveries[indexPath.row].name
+        return cell
     }
 
 }
