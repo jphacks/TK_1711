95a3c8b40032696ffaf9a8ec7939ebd972ae4bb4
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-10-28 20:45:59 +0900

    Add vc for login

---
+++ ios-client/my-block/my-block/ViewController/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  my-block
+//
+//  Created by Masaya Hayashi on 2017/10/28.
+//  Copyright © 2017年 Masaya Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
