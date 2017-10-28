93ca97695f45e12420abf2877e7bd9817538bdb4
keke <19yamashita15@gmail.com>
2017-10-28 13:41:02 +0900

    add show to containers

--- rails/app/controllers/api/v1/containers_controller.rb
+++ rails/app/controllers/api/v1/containers_controller.rb
@@ -3,6 +3,11 @@ class Api::V1::ContainersController < Api::V1::BaseController
         @containers = {"container":Container.all}
         render :json => @containers.to_json
     end
+
+    def show
+        @container = {"container":Container.find(params[:id])}
+        render :json => @container.to_json
+    end
     
     def new
     end
