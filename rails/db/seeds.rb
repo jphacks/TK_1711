fb6a0cfb9ed8253d951923afc462cd01e5b93e37
keke <19yamashita15@gmail.com>
2017-10-29 17:26:10 +0900

    final push

--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -16,12 +16,15 @@
 
 duration = rand(0..3)
 status = rand(0..2)
-user1 = User.create(name:"川上",address:"0xe8047fe4612bdaff46ef896cb87fff10a68b52ec", balance:5000)
+user1 = User.create(name:"川上",address:"0xe8047fe4612bdaff46ef896cb87fff10a68b52ec", balance:62350)
 containers1 = user1.containers.create(name:"ソファ",delivery_date:"2017-10-20",purchase_date:"2017-10-20",photo:"https://s3-ap-northeast-1.amazonaws.com/myblock/sofa.jpg",duration:duration,status:status)
+containers1 = user1.containers.create(name:"ティッシュペーパー",delivery_date:"2017-10-20",purchase_date:"2017-10-20",photo:"https://s3-ap-northeast-1.amazonaws.com/myblock/tissue.jpg",duration:duration,status:status)
+containers1 = user1.containers.create(name:"SOYJOY",delivery_date:"2017-10-20",purchase_date:"2017-10-20",photo:"https://s3-ap-northeast-1.amazonaws.com/myblock/soy.jpg",duration:duration,status:status)
 duration = rand(0..3)
 status = rand(0..2)
 
-user2 = User.create(name:"山田",address:"0x21ab4b628bd3af3bcfa38d18cf8de3d243026956",balance:8000)
+
+user2 = User.create(name:"山田",address:"0x21ab4b628bd3af3bcfa38d18cf8de3d243026956",balance:84020)
 for num in 1..5 do
     containers2 = user2.containers.create(name:"グラノーラ",delivery_date:"2017-10-20",purchase_date:"2017-10-20",photo:"https://s3-ap-northeast-1.amazonaws.com/myblock/gura.png",duration:duration,status:status)
 end
@@ -32,14 +35,17 @@ duration = rand(0..3)
 status = rand(0..2)
 containers2 = user2.containers.create(name:"ソファ",delivery_date:"2017-10-20",purchase_date:"2017-10-20",photo:"https://s3-ap-northeast-1.amazonaws.com/myblock/sofa.jpg",duration:duration,status:status)
 
+
+
+
 duration = rand(0..3)
 status = rand(0..2)
-user3 = User.create(name:"林",address:"0x786971589e24ca1d42ba813c0bd26936081b2085",balance:9000)
+user3 = User.create(name:"林",address:"0x786971589e24ca1d42ba813c0bd26936081b2085",balance:58203)
 containers3 = user3.containers.create(name:"プロテイン",delivery_date:"2017-10-20",purchase_date:"2017-10-20",photo:"https://s3-ap-northeast-1.amazonaws.com/myblock/protain.png",duration:duration,status:status)
 
 duration = rand(0..3)
 status = rand(0..2)
-user4 = User.create(name:"さくま農家",address:"0x0b9d052652f94547464746a08e8281cd7aa6e059",balance:6000)
+user4 = User.create(name:"さくま農家",address:"0x0b9d052652f94547464746a08e8281cd7aa6e059",balance:39264)
 containers4 = user4.containers.create(name:"7up",delivery_date:"2017-10-20", purchase_date:"2017-10-20",photo:"https://s3-ap-northeast-1.amazonaws.com/myblock/7up.jpg",duration:duration,status:status)
 duration = rand(0..3)
 status = rand(0..2)
